20f451c94e8adedc279672639299cf484f3daea4
EnzoFBS <enzo.silvestre@bandtec.com.br>
2021-11-02 18:16:39 -0300

    JS_86%

--- index.js
+++ index.js
@@ -21,105 +21,276 @@ function cruzadinha(){
    // ok
     
     if (palavra3 == "DUALCORE"){
+        td_dual_d.style = "background-color: green;"
+        td_dual_u.style = "background-color: green;"
+        td_dual_a.style = "background-color: green;"
+        td_dual_l.style = "background-color: green;"
+        td_dual_c.style = "background-color: green;"
+        td_eprom_dual_o.style = "background-color: green;"
+        td_dual_r.style = "background-color: green;"
+        td_dual_adres_e.style = "background-color: green;"
 
         msg.innerHTML += "Palavra 1: DUALCORE<br>"
     }else{
+
+        td_dual_d.style = "background-color: red;"
+        td_dual_u.style = "background-color: red;"
+        td_dual_a.style = "background-color: red;"
+        td_dual_l.style = "background-color: red;"
+        td_dual_c.style = "background-color: red;"
+        td_eprom_dual_o.style = "background-color: red;"
+        td_dual_r.style = "background-color: red;"
+        td_dual_adres_e.style = "background-color: red;"
+
+
         console.log("erro na  quadcore")
-        input.style.background = "green"
+    
 
     } 
 
     if (palavra7 == "EPROM"){
+
+        td_massa_eprom_e.style = "background-color: green"
+        td_eprom_p.style = "background-color: green"
+        td_eprom_r.style = "background-color: green"
+        td_eprom_dual_o.style = "background-color: green"
+        td_eprom_m.style = "background-color: green"
+        
         msg.innerHTML += "Palavra 2: EPROM <br>"
     }else{
+
+        td_massa_eprom_e.style = "background-color: red"
+        td_eprom_p.style = "background-color: red"
+        td_eprom_r.style = "background-color: red"
+        td_eprom_dual_o.style = "background-color: red"
+        td_eprom_m.style = "background-color: red"
+        
         console.log("erro na eprom")
     } 
 
 
     if(palavra15 == "REGISTRADORES"){
+
+        td_regis_r_1.style = "background-color: green"
+        td_regis_quad_e.style = "background-color: green"
+        td_regis_g.style = "background-color: green"
+        td_regis_i5_i.style = "background-color: green"
+        td_regis_s.style = "background-color: green"
+        td_regis_t.style = "background-color: green"
+        td_regis_r.style = "background-color: green"
+        td_regis_a.style = "background-color: green"
+        td_regis_d.style = "background-color: green"
+        td_regis_o.style = "background-color: green"
+        td_regis_r.style = "background-color: green"
+        td_regis_e.style = "background-color: green"
+        td_regis_s.style = "background-color: green"
+        
+
+
+       
         msg.innerHTML += "Palavra 3: registradores <br>"
     }else{
+
+        td_regis_r_1.style = "background-color: red"
+        td_regis_quad_e.style = "background-color: red"
+        td_regis_g.style = "background-color: red"
+        td_regis_i5_i.style = "background-color: red"
+        td_regis_s.style = "background-color: red"
+        td_regis_t.style = "background-color: red"
+        td_regis_r.style = "background-color: red"
+        td_regis_a.style = "background-color: red"
+        td_regis_d.style = "background-color: red"
+        td_regis_o.style = "background-color: red"
+        td_regis_r.style = "background-color: red"
+        td_regis_e.style = "background-color: red"
+        td_regis_s.style = "background-color: red"
+
         console.log("erro na registradores")
     }
 
     if(palavra4 == "CPU"){
-        validacao_correta()
+
+        td_cpu_c.style = "background-color: green"
+        td_cpu_p.style = "background-color: green"
+        td_cpu_ula_u.style = "background-color: green"
+
+        
         msg.innerHTML += "Palavra 4: CPU <br>"
     }else{
-        validacao_incorreta()
+
+        td_cpu_c.style = "background-color: red"
+        td_cpu_p.style = "background-color: red"
+        td_cpu_ula_u.style = "background-color: red"
+       
         console.log("erro na CPU")
     }
 
     if(palavra8 == "QUADCORE"){
+
+        td_quad_q.style =  "background-color: green"
+        td_quad_u.style =  "background-color: green"
+        td_data_quad_a.style = "background-color: green"
+        td_quad_d.style = "background-color: green"
+        td_quad_c.style = "background-color: green"
+        td_rom_quad_o.style = "background-color: green"
+        td_quad_r.style = "background-color: green"
+        td_regis_quad_e.style = "background-color: green" 
+
+     
         msg.innerHTML += "Palavra 5: QUADCORE <br>"
     }else{
+
+        td_quad_q.style =  "background-color: red"
+        td_quad_u.style =  "background-color: red"
+        td_data_quad_a.style = "background-color: red"
+        td_quad_d.style = "background-color: red"
+        td_quad_c.style = "background-color: red"
+        td_rom_quad_o.style = "background-color: red"
+        td_quad_r.style = "background-color: red"
+        td_regis_quad_e.style = "background-color: red" 
+
         console.log("erro na QUADCORE <br>")
     }
 
     if(palavra9 == "CS"){
+      
+        td_cs_c.style = "background-color: green" 
+        td_regis_cs_s.style = "background-color: green" 
+
         msg.innerHTML += "Palavra 6: CS <br>"
     }else{
+
+        td_cs_c.style = "background-color: red" 
+        td_regis_cs_s.style = "background-color: red" 
+
         console.log("erro na cs")
     }
 
     if(palavra16 == "I5"){
+
+        td_regis_i5_i.style = "background-color: green"
+        td_i5_5.style =  "background-color: green" 
+
+        
         msg.innerHTML += "Palavra 7: i5 <br>"
     }else{
+        
+        td_regis_i5_i.style = "background-color: red"
+        td_i5_5.style =  "background-color: red" 
         console.log("erro no i5")
     }
 
     if(palavra12 == "DATABUS"){
+
+        td_data_d.style = "background-color: green"
+        td_data_quad_a.style = "background-color: green"
+        td_data_t.style = "background-color: green"
+        td_data_ram_a.style = "background-color: green"
+        td_data_b.style = "background-color: green"
+        td_data_u.style = "background-color: green"
+        td_data_massa_s.style = "background-color: green"
+
+        
         msg.innerHTML += "Palavra 8: databus<br>"
     }else{
+
+        td_data_d.style = "background-color: red"
+        td_data_quad_a.style = "background-color: red"
+        td_data_t.style = "background-color: red"
+        td_data_ram_a.style = "background-color: red"
+        td_data_b.style = "background-color: red"
+        td_data_u.style = "background-color: red"
+        td_data_massa_s.style = "background-color: red"
+
         console.log("erro no databus")
     }
 
     if(palavra1 == "MEMORIADEMASSA"){
+
+        td_massa_m.style = "background-color: green"
+        td_massa_e.style = "background-color: green"
+        td_dma_massa_m.style = "background-color: green"
+        td_massa_o.style= "background-color: green"
+        td_massa_r.style= "background-color: green"
+        td_massa_i7_i.style = "background-color: green"
+        td_massa_ula_a.style = "background-color: green"
+        td_massa_d.style = "background-color: green"
+        td_massa_e.style = "background-color: green"
+        td_massa_m_3.style = "background-color: green"
+        td_massa_adres_a.style = "background-color: green"
+        td_data_massa_s.style = "background-color: green"
+        td_massa_s.style = "background-color: green"
+        td_flash_massa_a.style = "background-color: green"
+
+        
         msg.innerHTML += "Palavra 9: memória de massa<br>"
     }else{
+
+        td_massa_m.style = "background-color: red"
+        td_massa_e.style = "background-color: red"
+        td_dma_massa_m.style = "background-color: red"
+        td_massa_o.style= "background-color: red"
+        td_massa_r.style= "background-color: red"
+        td_massa_i7_i.style = "background-color: red"
+        td_massa_ula_a.style = "background-color: red"
+        td_massa_d.style = "background-color: red"
+        td_massa_e.style = "background-color: red"
+        td_massa_m_3.style = "background-color: red"
+        td_massa_adres_a.style = "background-color: red"
+        td_data_massa_s.style = "background-color: red"
+        td_massa_s.style = "background-color: red"
+        td_flash_massa_a.style = "background-color: red"
+
+
         console.log("erro na memoria de massa")
     }
 
     if(palavra5 == "I7"){
+        
         msg.innerHTML += "Palavra 10: i7 <br>"
     }else{
         console.log("erro no i7")
     }
 
     if(palavra2 == "DMA"){
+        
         msg.innerHTML += "Palavra 11: dma <br>"
     }else{
         console.log("erro na dma")
     }
 
     if(palavra10 == "RAM"){
+        
         msg.innerHTML += "Palavra 12: ram <br>"
     } else {
         console.log("erro na ram")
     }
 
     if(palavra13 == "FLASH"){
+        
         msg.innerHTML += "Palavra 13: flash <br>"
     }else{
         console.log("erro na flash")
     }
 
     if(palavra11 == "ADRESBUSS"){
+        
         msg.innerHTML += "Palavra 14: adresbuss <br>"
     }else{
         console.log("erro na adresbuss")
     }
 
     if(palavra14 == "ROM"){
-        validacao_correta()
+        
         msg.innerHTML += "Palavra 15: rom <br>"
     }else{
 
-        validacao_incorreta()
+       
         console.log("erro na rom")
     }
 
     if(palavra6 == "ULA"){
+       
         msg.innerHTML += "Palavra 16: ula <br>"
     }else{
         console.log("erro na ula")
@@ -130,14 +301,15 @@ function cruzadinha(){
 
 
 function validacao_correta() {
-    
-    I
-  
+
+   
     
 }
 
 function validacao_incorreta() {
 
-  
+    
+
+
 
 }   
